4a668faced2d4664554edbf57193b3757d1386e0
Deniz Yuret <denizyuret@gmail.com>
2004-12-08 13:53:41 +0000

    fixed little bug with dot.

--- procinfo.h
+++ procinfo.h
@@ -4,7 +4,9 @@
 #include <glib.h>
 
 extern int procinfo_cnt;
-#define dot(n) (((n)==0) ? procinfo_cnt = (fputc('\n', stderr), 0) : ((++procinfo_cnt)%(n>>5) == 0) ? fputc('.', stderr) : 0)
+#define dot(n) (((n)==0) ? procinfo_cnt = (fputc('\n', stderr), 0) :\
+		((++procinfo_cnt) && ((n)<32)) ? fputc('.', stderr) :\
+		((procinfo_cnt)%((n)>>5) == 0) ? fputc('.', stderr) : 0)
 
 void my_log_func(const gchar *log_domain,
 		 GLogLevelFlags log_level,
