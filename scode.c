3d61ddc29839607de2c29325a970ff1df5d07069
Deniz Yuret <denizyuret@gmail.com>
2014-01-12 16:19:14 +0200

    First glib-free version.

--- scode.c
+++ scode.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #include <unistd.h>
-#include <glib.h>
+#include <assert.h>
 #include <math.h>
 #include <gsl/gsl_vector.h>
 #include <gsl/gsl_rng.h>
-#include "foreach.h"
-#include "procinfo.h"
+#include "dlib.h"
 #include "svec.h"
 #include "rng.h"
 
@@ -28,7 +27,7 @@ const char *usage = "Usage: scode [OPTIONS] < file\n"
 
 int NTOK = -1;
 int RESTART = 1;
-int NITER = INT_MAX;
+int NITER = UINT32_MAX;
 double THRESHOLD = 0.001;
 int NDIM = 25;
 double Z = 0.166;
@@ -45,19 +44,18 @@ int PRINTALL = 0;
 
 const gsl_rng_type *rng_T;
 gsl_rng *rng_R = NULL;
-GArray *data;
-#define DATA_MAXLEN (1<<29)
-guint **update_cnt;
-gdouble * weight = NULL;
-gdouble * uweight = NULL; /*Updated weights*/
-guint * cweight = NULL; /*Weights Counts*/
-guint **cnt;
-#define frq(i,j) ((double)cnt[i][j]*NTOK/data->len)
+darr_t data;
+uint32_t **update_cnt;
+double * weight = NULL;
+double * uweight = NULL; /*Updated weights*/
+uint32_t * cweight = NULL; /*Weights Counts*/
+uint32_t **cnt;
+#define frq(i,j) ((double)cnt[i][j]*NTOK/len(data))
 svec **vec;
 svec **best_vec;
 svec dummy_vec;
-GQuark qmax;
-GQuark NULLFEATID;
+sym_t qmax;
+sym_t NULLFEATID;
 #define NULLFEATMARKER "/XX/"
 
 int main(int argc, char **argv);
@@ -68,11 +66,11 @@ int init_weight();
 void randomize_vectors();
 void copy_best_vec();
 void free_data();
-void update_tuple(GQuark *t);
+void update_tuple(sym_t *t);
 double logL();
 double calcZ();
 
-#define msg if(VERBOSE)g_message
+#define vmsg(...) if(VERBOSE)msg(__VA_ARGS__)
 
 int main(int argc, char **argv) {
   int opt;
@@ -88,16 +86,15 @@ int main(int argc, char **argv) {
     case 's': SEED = atoi(optarg); break;
     case 'e': EXPER = atoi(optarg); break;
     case 'c': CALCZ = 1; break;
-    case 'm': VMERGE = 1; msg("The -m option is deprecated, please do not use."); break;
+    case 'm': VMERGE = 1; vmsg("The -m option is deprecated, please do not use."); break;
     case 'w': WEIGHT = 1; break;
     case 'v': VERBOSE = 1; break;
     case 'a': PRINTALL = 1; break;
-    default: g_error("%s",usage);
+    default: die("%s",usage);
     }
   }
 
-  g_message_init();
-  msg("scode -n %d -r %d -i %d -t %g -d %d -z %g -u %g -p %g -s %d %s%s%s",
+  vmsg("scode -n %d -r %d -i %d -t %g -d %d -z %g -u %g -p %g -s %d %s%s%s",
       NTOK, RESTART, NITER, THRESHOLD, NDIM, Z, NU0, PHI0, SEED,
       (CALCZ ? "-c " : ""), (VMERGE ? "-m " : ""), (VERBOSE ? "-v " : ""));
 
@@ -105,37 +102,37 @@ int main(int argc, char **argv) {
   if (SEED) gsl_rng_set(rng_R, SEED);
   if (WEIGHT) NTOK = init_weight();
   NTUPLE = init_data();
-  msg("Read %d tuples %d uniq tokens", NTUPLE, qmax);
+  vmsg("Read %d tuples %d uniq tokens", NTUPLE, qmax);
 
   double best_logL = 0;
   for (int start = 0; start < RESTART; start++) {
     randomize_vectors();
     double ll = logL();
-    msg("Restart %d/%d logL0=%g best=%g", 1+start, RESTART, ll, best_logL);
-    if (CALCZ) msg("Z=%g (approx %g)", calcZ(), Z);
+    vmsg("Restart %d/%d logL0=%g best=%g", 1+start, RESTART, ll, best_logL);
+    if (CALCZ) vmsg("Z=%g (approx %g)", calcZ(), Z);
     for (int iter = 0; iter < NITER; iter++) {
       for (int di = 0; di < NTUPLE; di++) {
-	update_tuple(&g_array_index(data, GQuark, di * NTOK));
+	update_tuple(&val(data, di * NTOK, sym_t));
       }
       double ll0 = ll;
       ll = logL();
-      msg("Iteration %d/%d logL=%g", 1+iter, NITER, ll);
+      vmsg("Iteration %d/%d logL=%g", 1+iter, NITER, ll);
       if (ll - ll0 <= THRESHOLD) break;
     }
     if (start == 0 || ll > best_logL) {
-      msg("Updating best_vec with logL=%g", ll);
+      vmsg("Updating best_vec with logL=%g", ll);
       best_logL = ll;
       copy_best_vec();
     }
-    msg("Restart %d/%d logL1=%g best=%g", 1+start, RESTART, ll, best_logL);
-    if (CALCZ) msg("Z=%g (approx %g)", calcZ(), Z);
+    vmsg("Restart %d/%d logL1=%g best=%g", 1+start, RESTART, ll, best_logL);
+    if (CALCZ) vmsg("Z=%g (approx %g)", calcZ(), Z);
   }
   if (VMERGE) {			/* output for Maron et.al. 2010 bigram s-code model */
     /* Actually there is no need for this, a script can do this with normal output. */
-    for (guint q = 1; q <= qmax; q++) {
-      printf("%s\t%d", g_quark_to_string(q), cnt[0][q]);
-      for (guint t = 0; t < NTOK; t++) {
-	g_assert(best_vec[t][q] != NULL);
+    for (uint32_t q = 1; q <= qmax; q++) {
+      printf("%s\t%d", sym2str(q), cnt[0][q]);
+      for (uint32_t t = 0; t < NTOK; t++) {
+	assert(best_vec[t][q] != NULL);
 	putchar('\t');
 	svec_print(best_vec[t][q]);
       }
@@ -144,10 +141,10 @@ int main(int argc, char **argv) {
   } else if(EXPER){
     fprintf(stderr,"<<<Experimental features: %d X + Y>>>\n", EXPER);
     for (int di = 0; di < NTUPLE; di++) {
-      GQuark *tar = &g_array_index(data, GQuark, di * NTOK);
-      for (guint t = 0; t < NTOK; t++) {
+      sym_t *tar = &val(data, di * NTOK, sym_t);
+      for (uint32_t t = 0; t < NTOK; t++) {
 	int q = tar[t];
-	g_assert(best_vec[t][q] != NULL);
+	assert(best_vec[t][q] != NULL);
 	putchar('\t');
 	if (t == 0){                         
 	  svec_mul_print(best_vec[t][q], EXPER);
@@ -161,10 +158,10 @@ int main(int argc, char **argv) {
     }
   }
   else if(PRINTALL){
-    for (guint t = 0; t < NTOK; t++) {
-      for (guint q = 1; q <= qmax; q++) {
+    for (uint32_t t = 0; t < NTOK; t++) {
+      for (uint32_t q = 1; q <= qmax; q++) {
 	if (best_vec[t][q] == NULL) continue;
-	printf("%d:%s\t%d\t", t, g_quark_to_string(q), cnt[t][q]);
+	printf("%d:%s\t%d\t", t, sym2str(q), cnt[t][q]);
 	svec_print(best_vec[t][q]);
 	putchar('\n');
       }
@@ -172,10 +169,10 @@ int main(int argc, char **argv) {
   }
   else {			/* regular output */
     //NTOK is set to 1
-    for (guint t = 0; t < 1; t++) {
-      for (guint q = 1; q <= qmax; q++) {
+    for (uint32_t t = 0; t < 1; t++) {
+      for (uint32_t q = 1; q <= qmax; q++) {
 	if (best_vec[t][q] == NULL) continue;
-	printf("%d:%s\t%d\t", t, g_quark_to_string(q), cnt[t][q]);
+	printf("%d:%s\t%d\t", t, sym2str(q), cnt[t][q]);
 	svec_print(best_vec[t][q]);
 	putchar('\n');
       }
@@ -185,15 +182,15 @@ int main(int argc, char **argv) {
   free_data();
   free_rng();
   fprintf(stderr, "%f\n", best_logL);
-  msg("bye");
+  vmsg("bye");
 }
 
 double logL() {
   double l = 0;
   for (int i = 0; i < NTUPLE; i++) {
-    GQuark *t = &g_array_index(data, GQuark, i * NTOK);
-    GQuark x = t[0];
-    GQuark y = t[1];
+    sym_t *t = &val(data, i * NTOK, sym_t);
+    sym_t x = t[0];
+    sym_t y = t[1];
     float px = frq(0, x);
     float py = frq(1, y);
     svec vx = vec[0][x];
@@ -206,12 +203,12 @@ double logL() {
 
 double calcZ() {
   double z = 0;
-  for (guint x = 1; x <= qmax; x++) {
+  for (uint32_t x = 1; x <= qmax; x++) {
     if (VERBOSE && (x % 1000 == 0)) fprintf(stderr, ".");
     if (cnt[0][x] == 0) continue;
     float px = frq(0, x);
     svec vx = vec[0][x];
-    for (guint y = 1; y <= qmax; y++) {
+    for (uint32_t y = 1; y <= qmax; y++) {
       if (cnt[1][y] == 0) continue;
       float py = frq(1, y);
       svec vy = vec[1][y];
@@ -223,7 +220,7 @@ double calcZ() {
   return z;
 }
 
-void update_tuple(GQuark *t) {
+void update_tuple(sym_t *t) {
   /*weighted update*/
   static svec *u = NULL;
   static svec *v = NULL;
@@ -238,18 +235,18 @@ void update_tuple(GQuark *t) {
     if(i > 0 && t[i] == NULLFEATID) continue;
     for (int j = 0; j < NTOK; j++) {
       if (j==i) { v[j] = u[i]; continue;}
-      guint r = gsl_rng_uniform_int(rng_R, NTUPLE);
-      GQuark y = g_array_index(data, GQuark, r * NTOK + j);
+      uint32_t r = gsl_rng_uniform_int(rng_R, NTUPLE);
+      sym_t y = val(data, r * NTOK + j, sym_t);
       v[j] = vec[j][y];
       if(i > 0) break;
     }          
     /* Compute the move for u[i] */
     svec_set_zero(dx);
-    gdouble ww;
+    double ww;
     for (int j = 0; j < NTOK; j++) {
       if (j == i) continue;
       ww = weight == NULL ? 1 : (i > 0 ? weight[i] : weight[j]);
-      gdouble push = 0, pull = 0;
+      double push = 0, pull = 0;
       if (v[j] == NULL) v[j] = dummy_vec;
       else push = exp(-svec_sqdist(u[i], v[j])) / Z;
       if(u[j] == NULL)  u[j] = dummy_vec;
@@ -269,7 +266,7 @@ void update_tuple(GQuark *t) {
       if(i > 0) break;
       }
     /* Apply the move scaled by learning parameter */
-    guint cx = update_cnt[i][t[i]]++;
+    uint32_t cx = update_cnt[i][t[i]]++;
     float nx = NU0 * (PHI0 / (PHI0 + cx));
     svec_scale(dx, nx);
     svec_add(u[i], dx);
@@ -278,14 +275,14 @@ void update_tuple(GQuark *t) {
 }
 
 #ifdef OLD
-void update_tuple_old(GQuark *t) {
+void update_tuple_old(sym_t *t) {
   static svec *u = NULL;
   static svec *v = NULL;
   static svec dx = NULL;
   if (u == NULL) u = malloc(NTOK * sizeof(svec));
   if (v == NULL) v = malloc(NTOK * sizeof(svec));
   if (dx == NULL) dx = svec_alloc(NDIM);
-  guint tok = NTOK;
+  uint32_t tok = NTOK;
   for (int i = 0; i < tok; i++) u[i] = vec[i][t[i]];
 
   for (int i = 0; i < tok; i++) {
@@ -294,19 +291,19 @@ void update_tuple_old(GQuark *t) {
     /* Can this be done once, or do we have to resample for every x? */
     for (int j = 0; j < tok; j++) {
       if (j==i) { v[j] = u[i]; continue; }
-      guint r = gsl_rng_uniform_int(rng_R, NTUPLE);
-      GQuark y = g_array_index(data, GQuark, r * NTOK + j);
+      uint32_t r = gsl_rng_uniform_int(rng_R, NTUPLE);
+      sym_t y = g_array_index(data, sym_t, r * NTOK + j);
       v[j] = vec[j][y];
     }
 
     /* Compute sum distance squared and the push coefficient */
-    gdouble d2 = 0;
+    double d2 = 0;
     for (int j = 0; j < tok; j++) {
       for (int k = 0; k < j; k++) {
 	d2 += svec_sqdist(v[k], v[j]);
       }
     }
-    gdouble push = exp(-d2) / Z;
+    double push = exp(-d2) / Z;
 
     /* Compute the move for u[i] */
     svec_set_zero(dx);
@@ -322,7 +319,7 @@ void update_tuple_old(GQuark *t) {
     }
     
     /* Apply the move scaled by learning parameter */
-    guint cx = update_cnt[i][t[i]]++;
+    uint32_t cx = update_cnt[i][t[i]]++;
     float nx = NU0 * (PHI0 / (PHI0 + cx));
     svec_scale(dx, nx);
     svec_add(u[i], dx);
@@ -331,19 +328,19 @@ void update_tuple_old(GQuark *t) {
 }
 
 
-float update_tuple_old(GQuark *t) {
-  GQuark x1 = t[0];
-  GQuark y1 = t[1];
-  guint cx = update_cnt[0][x1]++;
-  guint cy = update_cnt[1][y1]++;
+float update_tuple_old(sym_t *t) {
+  sym_t x1 = t[0];
+  sym_t y1 = t[1];
+  uint32_t cx = update_cnt[0][x1]++;
+  uint32_t cy = update_cnt[1][y1]++;
   float nx = NU0 * (PHI0 / (PHI0 + cx));
   float ny = NU0 * (PHI0 / (PHI0 + cy));
   svec vx1 = vec[0][x1];
   svec vy1 = vec[1][y1];
-  guint rx = gsl_rng_uniform_int(rng_R, NTUPLE);
-  GQuark x2 = g_array_index(data, GQuark, rx * NTOK);
-  guint ry = gsl_rng_uniform_int(rng_R, NTUPLE);
-  GQuark y2 = g_array_index(data, GQuark, ry * NTOK + 1);
+  uint32_t rx = gsl_rng_uniform_int(rng_R, NTUPLE);
+  sym_t x2 = g_array_index(data, sym_t, rx * NTOK);
+  uint32_t ry = gsl_rng_uniform_int(rng_R, NTUPLE);
+  sym_t y2 = g_array_index(data, sym_t, ry * NTOK + 1);
   svec vx2 = vec[0][x2];
   svec vy2 = vec[1][y2];
   float x1y2 = svec_sqdist(vx1, vy2);
@@ -374,17 +371,17 @@ float update_svec_old(svec x, svec y, svec y2, float xy2, float nx) {
   
 int init_weight(){
   int size = 100, i = 0;
-  weight = malloc(size * sizeof(gdouble));
-  foreach_line(buf, "") {
-    foreach_token(tok, buf) {
+  weight = _d_malloc(size * sizeof(double));
+  forline (buf, NULL) {
+    fortok (tok, buf) {
       weight[i] = atof(tok);
-      g_assert(weight[i++] >= 0);
+      assert(weight[i++] >= 0);
       if(i >= 100) {
 	size *= 2; 
-	weight = realloc(weight, size);
+	weight = _d_realloc(weight, size);
       }
     }
-    g_assert(i > 0);
+    assert(i > 0);
     break;
   }
   return i;
@@ -392,41 +389,40 @@ int init_weight(){
 
 int init_data() {
   qmax = 0;
-  data = g_array_new(FALSE, FALSE, sizeof(GQuark));
-  foreach_line(buf, "") {
-    int i = 0;
-    foreach_token(tok, buf) {
-      GQuark q = g_quark_from_string(tok);               
+  data = darr(0, sym_t);
+  forline (buf, NULL) {
+    size_t ntok = 0;
+    fortok (tok, buf) {
+      sym_t q = str2sym(tok, true);
       if (q > qmax) qmax = q;
-      if (data->len >= DATA_MAXLEN) g_error("Number of tokens exceeded the maximum of %d", DATA_MAXLEN);
-      g_array_append_val(data, q);
-      if(strcmp(tok,NULLFEATMARKER) == 0) NULLFEATID = q;
-      i++;
+      val(data, len(data), sym_t) = q;
+      if(strcmp(tok, NULLFEATMARKER) == 0) NULLFEATID = q;
+      ntok++;
     }
-    if(NTOK == -1) NTOK = i;
-    g_assert(i == NTOK); //Each line has equal number of tokens
+    if(NTOK == -1) NTOK = ntok;
+    assert(ntok == NTOK); //Each line has equal number of tokens
   }
-  g_assert(NTOK > 0);
-  update_cnt = malloc(NTOK * sizeof(gpointer));
-  cnt = malloc(NTOK * sizeof(gpointer));
-  vec = malloc(NTOK * sizeof(gpointer));
-  best_vec = malloc(NTOK * sizeof(gpointer));     
+  assert(NTOK > 0);
+  update_cnt = _d_malloc(NTOK * sizeof(ptr_t));
+  cnt = _d_malloc(NTOK * sizeof(ptr_t));
+  vec = _d_malloc(NTOK * sizeof(ptr_t));
+  best_vec = _d_malloc(NTOK * sizeof(ptr_t));     
   dummy_vec = svec_alloc(NDIM);
   svec_zero(dummy_vec);
-  uweight = calloc(NTOK, sizeof(gdouble));
-  cweight = calloc(NTOK, sizeof(guint));
+  uweight = _d_calloc(NTOK, sizeof(double));
+  cweight = _d_calloc(NTOK, sizeof(uint32_t));
   for (int i = 0; i < NTOK; i++) {
-    update_cnt[i] = g_new0(guint, qmax+1);
-    cnt[i] = g_new0(guint, qmax+1);
-    vec[i] = g_new0(svec, qmax+1);
-    best_vec[i] = g_new0(svec, qmax+1);
+    update_cnt[i] = _d_calloc(qmax+1, sizeof(uint32_t));
+    cnt[i] = _d_calloc(qmax+1, sizeof(uint32_t));
+    vec[i] = _d_calloc(qmax+1, sizeof(svec));
+    best_vec[i] = _d_calloc(qmax+1, sizeof(svec));
   }
-  int N = data->len / NTOK;
+  int N = len(data) / NTOK;
   for (int i = 0; i < N; i++) {
-    GQuark *p = &g_array_index(data, GQuark, i * NTOK);    
+    sym_t *p = &val(data, i * NTOK, sym_t);
     for (int j = 0; j < NTOK; j++) {
       int k = p[j];
-      g_assert(k <= qmax);
+      assert(k <= qmax);
       cnt[j][k]++;
       if(k == NULLFEATID){
 	vec[j][k] = best_vec[j][k] = NULL;
@@ -440,9 +436,35 @@ int init_data() {
   return N;
 }
 
+void free_data() {
+  for (int i = 0; i < NTOK; i++) {
+    for (int j = 0; j <= qmax; j++) {
+      if (vec[i][j] != NULL) {
+	svec_free(vec[i][j]);
+	svec_free(best_vec[i][j]);
+      }
+    }
+    _d_free(best_vec[i]);
+    _d_free(vec[i]);
+    // _d_free(cnt[i]); // TODO: why is this not here?
+    _d_free(update_cnt[i]);
+  }
+  _d_free(cweight);
+  _d_free(uweight);
+  svec_free(dummy_vec);
+  /* TODO: why are these not here:
+  _d_free(best_vec);
+  _d_free(vec);
+  _d_free(cnt);
+  _d_free(update_cnt);
+  */
+  _d_free(weight); // TODO: this has not been alloced in init_data, do not free here.
+  darr_free(data);
+}
+
 void randomize_vectors() {
   for (int j = 0; j < NTOK; j++) {
-    for (guint q = 1; q <= qmax; q++) {
+    for (uint32_t q = 1; q <= qmax; q++) {
       if (vec[j][q] != NULL) {
 	svec_randomize(vec[j][q]);
 	update_cnt[j][q] = 0;
@@ -453,31 +475,12 @@ void randomize_vectors() {
 
 void copy_best_vec() {
   for (int j = 0; j < NTOK; j++) {
-    for (guint q = 1; q <= qmax; q++) {
+    for (uint32_t q = 1; q <= qmax; q++) {
       if (vec[j][q] != NULL) {
 	svec_memcpy(best_vec[j][q], vec[j][q]);
       }
     }
   }
-}
-
-void free_data() {
-  for (int i = 0; i < NTOK; i++) {
-    for (int j = 0; j <= qmax; j++) {
-      if (vec[i][j] != NULL) {
-	svec_free(vec[i][j]);
-	svec_free(best_vec[i][j]);
-      }
-    }
-    g_free(vec[i]);
-    g_free(best_vec[i]);
-    g_free(update_cnt[i]);
-  }
-  g_array_free(data, TRUE);
-  svec_free(dummy_vec);
-  g_free(weight);
-  g_free(cweight);
-  g_free(uweight);
 }
 
 void init_rng() {
